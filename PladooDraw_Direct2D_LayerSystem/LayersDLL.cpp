ef9e8ca6a04ae480d0255282ec5792cf803dac60
Eduardo Soares <eduardo@pladoo.com.br>
2024-11-26 12:46:51 -0300

    Updates

--- PladooDraw_Direct2D_LayerSystem/LayersDLL.cpp
+++ PladooDraw_Direct2D_LayerSystem/LayersDLL.cpp
@@ -1,26 +1,41 @@
 // LayersDLL.cpp
 #include "pch.h"
 #include <windows.h>
-#include <d2d1.h>
+#include <wincodec.h>
 #include "LayersDLL.h"
 #include <vector>
 #include <sstream>
+#include <d2d1_1.h>
 
 struct Layer {
     ID2D1BitmapRenderTarget* pBitmapRenderTarget;
     ID2D1Bitmap* pBitmap;
 };
 
+IWICImagingFactory* pWICFactory = nullptr;
 ID2D1Factory* pD2DFactory = nullptr;
 ID2D1HwndRenderTarget* pRenderTarget = nullptr;
 ID2D1SolidColorBrush* pBrush = nullptr;
 ID2D1SolidColorBrush* transparentBrush = nullptr;
 
-D2D1_RECT_F prevRectangle = D2D1::Rect(0, 0, 0, 0);
+D2D1_ELLIPSE ellipse = D2D1::Ellipse(D2D1::Point2F(0, 0), 0, 0);
+D2D1_RECT_F rectangle = D2D1::RectF(0, 0, 0, 0);
+D2D1_RECT_F prevRectangle = D2D1::RectF(0, 0, 0, 0);
 D2D1_ELLIPSE prevEllipse = D2D1::Ellipse(D2D1::Point2F(0, 0), 0, 0);
+
+D2D1_POINT_2F startPoint = { 0, 0 };
+D2D1_POINT_2F endPoint = { 0, 0 };
+
+static int currentBrushSize = 0;
 
 static int prevLeft = -1;
 static int prevTop = -1;
+
+static bool isDrawingRectange = false;
+static bool isDrawingEllipse = false;
+static bool isDrawingLine = false;
+
+static unsigned int currentColor = 0;
 
 std::vector<Layer> layers;
 
@@ -43,6 +58,25 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
     }
 
     return TRUE;
+}
+
+ID2D1SolidColorBrush* D2_CreateSolidBrush(unsigned int hexColor) {
+    currentColor = hexColor;
+
+    if (lastHexColor == UINT_MAX || hexColor != lastHexColor) {
+        float r = (hexColor & 0xFF) / 255.0f;         
+        float g = ((hexColor >> 8) & 0xFF) / 255.0f;  
+        float b = ((hexColor >> 16) & 0xFF) / 255.0f; 
+        float a = 1.0f;                               
+
+        D2D1_COLOR_F color = {r, g, b, a};  
+
+        pRenderTarget->CreateSolidColorBrush(color, &pBrush);
+    }
+
+    lastHexColor = hexColor;
+
+    return pBrush;
 }
 
 extern "C" __declspec(dllexport) HRESULT Initialize(HWND hWnd) {
@@ -81,13 +115,6 @@ extern "C" __declspec(dllexport) HRESULT Initialize(HWND hWnd) {
     return S_OK;
 }
 
-extern "C" __declspec(dllexport) void ResetParameters() {
-    prevRectangle = D2D1::Rect(0, 0, 0, 0);
-    prevEllipse = D2D1::Ellipse(D2D1::Point2F(0, 0), 0, 0);
-    prevLeft = -1;
-    prevTop = -1;
-}
-
 extern "C" __declspec(dllexport) HRESULT AddLayer() {
     ID2D1BitmapRenderTarget* pBitmapRenderTarget = nullptr;
 
@@ -99,7 +126,7 @@ extern "C" __declspec(dllexport) HRESULT AddLayer() {
     );
 
     D2D1_BITMAP_PROPERTIES properties = D2D1::BitmapProperties(pixelFormat);
-    
+
     HRESULT renderTargetCreated = pRenderTarget->CreateCompatibleRenderTarget(
         D2D1::SizeF(size.width, size.height), &pBitmapRenderTarget);
 
@@ -122,6 +149,46 @@ extern "C" __declspec(dllexport) HRESULT AddLayer() {
     return S_OK;
 }
 
+extern "C" __declspec(dllexport) void handleMouseUp() {
+
+    if (isDrawingRectange || isDrawingEllipse || isDrawingLine) {
+        ID2D1SolidColorBrush* brush = D2_CreateSolidBrush(currentColor);
+
+        layers[layerIndex].pBitmapRenderTarget->BeginDraw();              
+
+        if (isDrawingRectange) {
+            layers[layerIndex].pBitmapRenderTarget->PushAxisAlignedClip(rectangle, D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
+            layers[layerIndex].pBitmapRenderTarget->FillRectangle(rectangle, brush);
+            layers[layerIndex].pBitmapRenderTarget->PopAxisAlignedClip();
+            rectangle = D2D1::RectF(0, 0, 0, 0);
+            isDrawingRectange = false;
+        }
+        
+        if (isDrawingEllipse) {
+            layers[layerIndex].pBitmapRenderTarget->FillEllipse(ellipse, brush);
+            ellipse = D2D1::Ellipse(D2D1::Point2F(0, 0), 0, 0);
+            isDrawingEllipse = false;
+        }
+
+        if (isDrawingLine) {
+            layers[layerIndex].pBitmapRenderTarget->DrawLine(startPoint, endPoint, brush, currentBrushSize, nullptr);
+            isDrawingLine = false;
+        }
+               
+        layers[layerIndex].pBitmapRenderTarget->EndDraw();
+
+        layers.pop_back();
+    }
+
+    startPoint = D2D1::Point2F(0, 0);
+    endPoint = D2D1::Point2F(0, 0);
+ 
+    prevRectangle = D2D1::RectF(0, 0, 0, 0);
+    prevEllipse = D2D1::Ellipse(D2D1::Point2F(0, 0), 0, 0);
+    prevLeft = -1;
+    prevTop = -1;
+}
+
 extern "C" __declspec(dllexport) void SetLayer(int index) {
     layerIndex = index;
     return;
@@ -129,31 +196,6 @@ extern "C" __declspec(dllexport) void SetLayer(int index) {
 
 extern "C" __declspec(dllexport) int LayersCount() {
     return layers.size();
-}
-
-HBITMAP ConvertD2DBitmapToHBitmap(ID2D1Bitmap* pD2DBitmap, HDC hdc)
-{
-    if (!pD2DBitmap || !hdc)
-        return nullptr;
-
-    D2D1_SIZE_U size = pD2DBitmap->GetPixelSize();
-
-    BITMAPINFO bmpInfo = {};
-    bmpInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-    bmpInfo.bmiHeader.biWidth = size.width;
-    bmpInfo.bmiHeader.biHeight = -static_cast<LONG>(size.height);
-    bmpInfo.bmiHeader.biPlanes = 1;
-    bmpInfo.bmiHeader.biBitCount = 32;
-    bmpInfo.bmiHeader.biCompression = BI_RGB;
-
-    void* pBits = nullptr;
-    HBITMAP hBitmap = CreateDIBSection(hdc, &bmpInfo, DIB_RGB_COLORS, &pBits, nullptr, 0);
-    
-    if (!hBitmap) {
-        return nullptr;
-    }
-
-    return hBitmap;
 }
 
 extern "C" __declspec(dllexport) void RenderLayers() {
@@ -176,25 +218,7 @@ extern "C" __declspec(dllexport) void RenderLayers() {
     }
 }
 
-ID2D1SolidColorBrush* D2_CreateSolidBrush(unsigned int hexColor) {
-
-    if (lastHexColor == UINT_MAX || hexColor != lastHexColor) {
-        float r = (hexColor & 0xFF) / 255.0f;         
-        float g = ((hexColor >> 8) & 0xFF) / 255.0f;  
-        float b = ((hexColor >> 16) & 0xFF) / 255.0f; 
-        float a = 1.0f;                               
-
-        D2D1_COLOR_F color = {r, g, b, a};  
-
-        pRenderTarget->CreateSolidColorBrush(color, &pBrush);
-    }
-
-    lastHexColor = hexColor;
-
-    return pBrush;
-}
-
-extern "C" __declspec(dllexport) void BrushTool(HDC hdc, int left, int top, unsigned int hexColor, int brushSize) {
+extern "C" __declspec(dllexport) void BrushTool(int left, int top, unsigned int hexColor, int brushSize) {
     ID2D1SolidColorBrush* brush = D2_CreateSolidBrush(hexColor);
 
     if (prevLeft == -1 && prevTop == -1) {
@@ -235,40 +259,51 @@ extern "C" __declspec(dllexport) void BrushTool(HDC hdc, int left, int top, unsi
     prevTop = top;
 }
 
-extern "C" __declspec(dllexport) void RectangleTool(HDC hdc, int left, int top, int right, int bottom, unsigned int hexColor) {
+extern "C" __declspec(dllexport) void RectangleTool(int left, int top, int right, int bottom, unsigned int hexColor) {
     ID2D1SolidColorBrush* brush = D2_CreateSolidBrush(hexColor);
 
-    layers[layerIndex].pBitmapRenderTarget->BeginDraw();
-
-    D2D1_RECT_F rectangle = D2D1::Rect(left, top, right, bottom);
-
-    if(prevRectangle.left != 0 || prevRectangle.top != 0 || prevRectangle.right != 0 ||prevRectangle.bottom != 0) {
-
-        layers[layerIndex].pBitmapRenderTarget->PushAxisAlignedClip(prevRectangle, D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
-
-        layers[layerIndex].pBitmapRenderTarget->Clear(D2D1::ColorF(0, 0, 0, 0));
-
-        layers[layerIndex].pBitmapRenderTarget->PopAxisAlignedClip();
+    if (!isDrawingRectange) {
+        AddLayer();
     }
 
-    layers[layerIndex].pBitmapRenderTarget->PushAxisAlignedClip(rectangle, D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
+    isDrawingRectange = true;
 
-    layers[layerIndex].pBitmapRenderTarget->FillRectangle(rectangle, brush);
+    layers[LayersCount() - 1].pBitmapRenderTarget->BeginDraw();
 
-    layers[layerIndex].pBitmapRenderTarget->PopAxisAlignedClip();
+    rectangle = D2D1::RectF(left, top, right, bottom);
 
-    prevRectangle = D2D1::Rect(left, top, right, bottom);
+    if (prevRectangle.left != 0 || prevRectangle.top != 0 || prevRectangle.right != 0 || prevRectangle.bottom != 0) {
 
-    layers[layerIndex].pBitmapRenderTarget->EndDraw();
+        layers[LayersCount() - 1].pBitmapRenderTarget->PushAxisAlignedClip(prevRectangle, D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
+
+        layers[LayersCount() - 1].pBitmapRenderTarget->Clear(D2D1::ColorF(0, 0, 0, 0));
+
+        layers[LayersCount() - 1].pBitmapRenderTarget->PopAxisAlignedClip();
+    }
+
+    layers[LayersCount() - 1].pBitmapRenderTarget->PushAxisAlignedClip(rectangle, D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
+
+    layers[LayersCount() - 1].pBitmapRenderTarget->FillRectangle(rectangle, brush);
+
+    layers[LayersCount() - 1].pBitmapRenderTarget->PopAxisAlignedClip();
+
+    prevRectangle = D2D1::RectF(left, top, right, bottom);
+
+    layers[LayersCount() - 1].pBitmapRenderTarget->EndDraw();
 }
 
-extern "C" __declspec(dllexport) void EllipseTool(HDC hdc, int left, int top, int right, int bottom, unsigned int hexColor) {
+extern "C" __declspec(dllexport) void EllipseTool(int left, int top, int right, int bottom, unsigned int hexColor) {
     ID2D1SolidColorBrush* brush = D2_CreateSolidBrush(hexColor);
+    
+    if (!isDrawingEllipse) {
+        AddLayer();
+    }
 
-    layers[layerIndex].pBitmapRenderTarget->BeginDraw();
+    isDrawingEllipse = true;
 
+    layers[LayersCount() - 1].pBitmapRenderTarget->BeginDraw();
 
-    D2D1_ELLIPSE ellipse = D2D1::Ellipse(D2D1::Point2F(left, top), abs(right - left), abs(bottom - top));
+    ellipse = D2D1::Ellipse(D2D1::Point2F(left, top), abs(right - left), abs(bottom - top));
 
     if (prevEllipse.point.x != 0 || prevEllipse.point.y != 0 || prevEllipse.radiusX != 0 || prevEllipse.radiusY != 0) {
         
@@ -279,30 +314,58 @@ extern "C" __declspec(dllexport) void EllipseTool(HDC hdc, int left, int top, in
             prevEllipse.point.y + prevEllipse.radiusY
         );
 
-        layers[layerIndex].pBitmapRenderTarget->PushAxisAlignedClip(prevRect, D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
+        layers[LayersCount() - 1].pBitmapRenderTarget->PushAxisAlignedClip(prevRect, D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
 
-        layers[layerIndex].pBitmapRenderTarget->Clear(D2D1::ColorF(0, 0, 0, 0));
+        layers[LayersCount() - 1].pBitmapRenderTarget->Clear(D2D1::ColorF(0, 0, 0, 0));
 
-        layers[layerIndex].pBitmapRenderTarget->PopAxisAlignedClip();
+        layers[LayersCount() - 1].pBitmapRenderTarget->PopAxisAlignedClip();
     }
 
-    D2D1_RECT_F rect = D2D1::RectF(
-        prevEllipse.point.x - prevEllipse.radiusX,
-        prevEllipse.point.y - prevEllipse.radiusY,
-        prevEllipse.point.x + prevEllipse.radiusX,
-        prevEllipse.point.y + prevEllipse.radiusY
-    );
-
-    layers[layerIndex].pBitmapRenderTarget->PushAxisAlignedClip(rect, D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
-    layers[layerIndex].pBitmapRenderTarget->FillEllipse(ellipse, brush);
-    layers[layerIndex].pBitmapRenderTarget->PopAxisAlignedClip();
+    layers[LayersCount() - 1].pBitmapRenderTarget->FillEllipse(ellipse, brush);
 
     prevEllipse = ellipse;
 
-    layers[layerIndex].pBitmapRenderTarget->EndDraw();
+    layers[LayersCount() - 1].pBitmapRenderTarget->EndDraw();
 }
 
-extern "C" __declspec(dllexport) void EraserTool(HDC hdc, int left, int top, int brushSize) {
+extern "C" __declspec(dllexport) void LineTool(int xInitial, int yInitial, int x, int y, unsigned int hexColor, int brushSize) {
+    ID2D1SolidColorBrush* brush = D2_CreateSolidBrush(hexColor);
+    currentBrushSize = brushSize;
+
+    if (!isDrawingLine) {
+        AddLayer();
+    }
+
+    isDrawingLine = true;
+    
+    layers[LayersCount() - 1].pBitmapRenderTarget->BeginDraw();
+
+    if (startPoint.x != 0 || startPoint.y != 0 || endPoint.x != 0 || endPoint.y != 0) {
+        D2D1_RECT_F lineBounds = D2D1::RectF(
+            min(startPoint.x, endPoint.x) - brushSize,
+            min(startPoint.y, endPoint.y) - brushSize,
+            max(startPoint.x, endPoint.x) + brushSize,
+            max(startPoint.y, endPoint.y) + brushSize
+        );
+
+        layers[LayersCount() - 1].pBitmapRenderTarget->PushAxisAlignedClip(lineBounds, D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
+        layers[LayersCount() - 1].pBitmapRenderTarget->Clear(D2D1::ColorF(0, 0, 0, 0));
+        layers[LayersCount() - 1].pBitmapRenderTarget->PopAxisAlignedClip();
+    }
+
+    startPoint = D2D1::Point2F(xInitial, yInitial);
+    endPoint = D2D1::Point2F(x, y);
+
+    layers[LayersCount() - 1].pBitmapRenderTarget->DrawLine(startPoint, endPoint, brush, brushSize, nullptr);
+
+    layers[LayersCount() - 1].pBitmapRenderTarget->EndDraw();
+}
+
+extern "C" __declspec(dllexport) void PaintBucketTool(int xInitial, int yInitial, unsigned int hexFillColor) {
+    return;
+}
+
+extern "C" __declspec(dllexport) void EraserTool(int left, int top, int brushSize) {
     auto target = layers[layerIndex].pBitmapRenderTarget;
 
     target->BeginDraw();
@@ -322,7 +385,6 @@ extern "C" __declspec(dllexport) void EraserTool(HDC hdc, int left, int top, int
 
     target->EndDraw();
 }
-
 
 extern "C" __declspec(dllexport) void Cleanup() {
     SafeRelease(&pBrush);
--- PladooDraw_Direct2D_LayerSystem/LayersDLL.h
+++ PladooDraw_Direct2D_LayerSystem/LayersDLL.h
@@ -3,14 +3,18 @@
 #include <d2d1.h>
 
 extern "C" __declspec(dllexport) HRESULT Initialize(HWND hWnd);
-extern "C" __declspec(dllexport) void ResetParameters();
+extern "C" __declspec(dllexport) void handleMouseUp();
 extern "C" __declspec(dllexport) HRESULT AddLayer();
 extern "C" __declspec(dllexport) void SetLayer(int index);
 extern "C" __declspec(dllexport) void RenderLayers();
 extern "C" __declspec(dllexport) int LayersCount();
-extern "C" __declspec(dllexport) void EllipseTool(HDC hdc, int left, int top, int right, int bottom, unsigned int hexColor);
-extern "C" __declspec(dllexport) void RectangleTool(HDC hdc, int left, int top, int right, int bottom, unsigned int hexColor);
-extern "C" __declspec(dllexport) void BrushTool(HDC hdc, int left, int top, unsigned int hexColor, int brushSize);
-extern "C" __declspec(dllexport) void EraserTool(HDC hdc, int left, int top, int brushSize);
+extern "C" __declspec(dllexport) void BrushTool(int left, int top, unsigned int hexColor, int brushSize);
+extern "C" __declspec(dllexport) void RectangleTool(int left, int top, int right, int bottom, unsigned int hexColor);
+extern "C" __declspec(dllexport) void EllipseTool(int left, int top, int right, int bottom, unsigned int hexColor);
+extern "C" __declspec(dllexport) void LineTool(int xInitial, int yInitial, int x, int y, unsigned int hexColor, int brushSize);
+extern "C" __declspec(dllexport) void PaintBucketTool(int xInitial, int yInitial, unsigned int hexFillColor);
+extern "C" __declspec(dllexport) void EraserTool(int left, int top, int brushSize);
 extern "C" __declspec(dllexport) void Cleanup();
+
+ID2D1SolidColorBrush* D2_CreateSolidBrush(unsigned int hexColor);
 template <class T> void SafeRelease(T** ppT);
