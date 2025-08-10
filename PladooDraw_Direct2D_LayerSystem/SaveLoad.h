54025f670d16918e0cf9b1a703e210b1abb4fbb4
eduardo-devmedia <eduscxbox@outlook.com>
2025-08-10 02:52:27 -0300

    Refatorado Save-Load
    Save e Load agora estão em seu próprio arquivo

---
+++ PladooDraw_Direct2D_LayerSystem/SaveLoad.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "Base.h"
+
+extern void SaveBinaryProject(const std::wstring& filename);
+extern void LoadBinaryProject(const std::wstring& filename, HWND hWndLayer, HINSTANCE hLayerInstance, int btnWidth, int btnHeight, HWND* hLayerButtons, int& layerID, const wchar_t* szButtonClass, const wchar_t* msgText);
